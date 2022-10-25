92adb0cc1402fd7bb0c9bcc92a03dd67202386c0
IUTA <vasavaseckin801@gmail.com>
2022-10-25 13:34:16 +0300

    correcting errors in the model

---
+++ mainapp/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+User = get_user_model()
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Имя категории')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Изображение')
+    description = models.TextField(verbose_name='Описание', null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+
+        abstract = True
+
+
+'''
+# класс наследуется от Product
+class NotebookProduct(Product):
+    pass
+'''
+
+
+class CardProduct(models.Model):
+
+    user = models.ForeignKey('Castomer', verbose_name='Покупатель', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_prodects')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    qty = models.PositiveIntegerField(default=1)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
+
+    def __str__(self):
+        return "Продукты: {} (для корзины)".format(self.product.title)
+
+
+class Cart(models.Model):
+
+    owner = models.ForeignKey('Castomer', verbose_name='Владелец', on_delete=models.CASCADE)
+    products = models.ManyToManyField(CardProduct, blank=True, related_name='related_cart')
+    total_product = models.PositiveIntegerField(default=0)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Castomer(models.Model):
+
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, verbose_name='Номер телефона')
+    address = models.CharField(max_length=255, verbose_name='Адрес')
+
+    def __str__(self):
+        return 'Покупатель: {} {}'.format(self.user.first_name, self.user.last_name)
+
+'''
+class Specifications(models.Model):
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=255, verbose_name='Имя товара для характеристик')
+
+    def __str__(self):
+        return 'Характеристики для товара: {}'.format(self.name)
+'''
