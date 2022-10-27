3754f2aae6e0ce7e639b92a35c6752c6e1a62dc2
IUTA <vasavaseckin801@gmail.com>
2022-10-27 10:49:31 +0300

    new models added and admin panel written

--- mainapp/models.py
+++ mainapp/models.py
@@ -34,13 +34,6 @@ class Product(models.Model):
         abstract = True
 
 
-'''
-# класс наследуется от Product
-class NotebookProduct(Product):
-    pass
-'''
-
-
 class CardProduct(models.Model):
 
     user = models.ForeignKey('Castomer', verbose_name='Покупатель', on_delete=models.CASCADE)
@@ -74,6 +67,37 @@ class Castomer(models.Model):
 
     def __str__(self):
         return 'Покупатель: {} {}'.format(self.user.first_name, self.user.last_name)
+
+
+# класс наследуется от Product
+class Notebook(Product):
+
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    processor_freq = models.CharField(max_length=255, verbose_name='Частота процессора')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    video = models.CharField(max_length=255, verbose_name='Видеокарта')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Время работы аккумулятора')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
+
+class Smartphone(Product):
+
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    resolution = models.CharField(max_length=255, verbose_name='Разрешение экрана')
+    accum_volume = models.CharField(max_length=255, verbose_name='Оъём батареии')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    sd = models.BooleanField(default=True)
+    sd_volume_max = models.CharField(max_length=255, verbose_name='Максимальный объём встраиваемой памяти')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Главная камера')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальная камера')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
 
 '''
 class Specifications(models.Model):
